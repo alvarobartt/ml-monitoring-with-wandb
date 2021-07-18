3d3c697b56d040e7fbc51acb4498725c5419632d
Alvaro Bartolome <alvarob96@usal.es>
2021-07-18 12:28:26 +0200

    prepared model_inference test (load from ckpt)

---
+++ src/inference.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Alvaro Bartolome, alvarobartt @ GitHub
+# See LICENSE for details.
+
+from __future__ import absolute_import
+
+from model import SimpsonsNet
+
+
+def model_inference():
+    pass
+
+
+if __name__ == '__main__':
+    model_inference()
